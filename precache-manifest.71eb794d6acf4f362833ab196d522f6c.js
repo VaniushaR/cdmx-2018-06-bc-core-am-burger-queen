1c90443b1fe142877aeb59b5831e0c75a8ab36c3
VaniushaR <vaniusha.ra@gmail.com>
2018-10-18 10:25:26 -0500

    Updates

---
+++ precache-manifest.71eb794d6acf4f362833ab196d522f6c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b135272f0a8bf3f330cb8730b1c2b036",
+    "url": "/static/media/logo.b135272f.jpg"
+  },
+  {
+    "revision": "83d105c30113b35cbb27c9a05c940cb9",
+    "url": "/static/media/Offer.83d105c3.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "7be58bf4cb2f5aac88c9",
+    "url": "/static/js/main.7be58bf4.chunk.js"
+  },
+  {
+    "revision": "c6472c041f98c5e23486",
+    "url": "/static/js/1.c6472c04.chunk.js"
+  },
+  {
+    "revision": "7be58bf4cb2f5aac88c9",
+    "url": "/static/css/main.f3951ab8.chunk.css"
+  },
+  {
+    "revision": "2d898e88a88d571cc1207e5371f4346d",
+    "url": "/index.html"
+  }
+];
